173138b0815f977bd69325d37e6d908fe09618a9
Shwet Chitnis <s2chitni@uw.edu>
2026-01-26 16:31:42 -0800

    porting over hb32 scheduler

---
+++ llvm/lib/Target/RISCV/HB32Scheduler.cpp
@@ -0,0 +1,93 @@
+//===-- HB32Scheduler.cpp - HB32 Subtarget specific passes -----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Implements HB32 Vanilla Core specific passes.
+//
+//===----------------------------------------------------------------------===//
+
+#include "RISCV.h"
+#include "RISCVInstrInfo.h"
+#include "HB32Scheduler.h"
+#include "llvm/Pass.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineScheduler.h"
+#include "llvm/CodeGen/MachineMemOperand.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "machine-scheduler"
+
+/// Flag to enable custom scheduler for HB32 Vanilla core
+static cl::opt<bool>
+    HB32Sched("hb32sched",
+              cl::desc("Enable HB32 Vanilla Core's custom scheduler"),
+              cl::init(false), cl::Hidden);
+
+/// Create custom scheduler if HB32Sched is enabled on the command line.
+ScheduleDAGInstrs *llvm::createHB32Scheduler(MachineSchedContext *C) {
+  if (HB32Sched) {
+    return new ScheduleDAGMILive(C, std::make_unique<HB32Scheduler>(C));
+  }
+
+  // NULL selects default machine scheduler
+  return nullptr;
+}
+
+/// HB32 Vanilla Core Scheduler
+SUnit *HB32Scheduler::pickNode (bool &IsTopNode) {
+  if (DAG->top() == DAG->bottom()) {
+    assert(Top.Available.empty() && Top.Pending.empty() &&
+           Bot.Available.empty() && Bot.Pending.empty() && "ReadyQ garbage");
+    return nullptr;
+  }
+
+  SUnit *SU;
+
+  do {
+    SU = Top.pickOnlyChoice();
+
+    // Priorotize loads from address space 1
+    if (!SU) {
+      for(SUnit* SUi : Top.Available) {
+        MachineInstr* MI = SUi->getInstr();
+        ArrayRef<MachineMemOperand*> memops = MI->memoperands();
+        if (MI->mayLoad() &&
+            !memops.empty() && memops[0]->getAddrSpace() == 1) {
+          LLVM_DEBUG(dbgs() << "HB32Scheduler bumping load ("
+                            << SUi->NodeNum << ") "
+                            << *MI);
+          SU = SUi;
+          break;
+        }
+      }
+    }
+
+    // Fallback to top-down policy of generic scheduler
+    if (!SU) {
+      CandPolicy NoPolicy;
+      TopCand.reset(NoPolicy);
+      pickNodeFromQueue(Top, NoPolicy, DAG->getTopRPTracker(), TopCand);
+      assert(TopCand.Reason != NoCand && "failed to find a candidate");
+
+      SU = TopCand.SU;
+    }
+    IsTopNode = true;
+  } while (SU->isScheduled);
+
+  if (SU->isTopReady())
+    Top.removeReady(SU);
+  if (SU->isBottomReady())
+    Bot.removeReady(SU);
+
+  LLVM_DEBUG(dbgs() << "Scheduling SU(" << SU->NodeNum << ") "
+                    << *SU->getInstr());
+  return SU;
+}
+
---
+++ llvm/lib/Target/RISCV/HB32Scheduler.h
@@ -0,0 +1,33 @@
+//===-- HB32Scheduler.h - HB32 Subtarget specific passes ----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// HB32 Vanilla Core specific passes.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_RISCV_VANILLASCHEDULER_H
+#define LLVM_LIB_TARGET_RISCV_VANILLASCHEDULER_H
+
+#include "llvm/CodeGen/MachineScheduler.h"
+
+namespace llvm {
+
+/// Creates HB32 Scheduler. This creates a custom scheduler in place of default
+/// scheduler if corresponding flag is provided on the command line.
+ScheduleDAGInstrs *createHB32Scheduler(MachineSchedContext *C);
+
+/// Custom scheduler for HB32 Vanilla Core extending the generic scheduler
+class HB32Scheduler : public GenericScheduler {
+public:
+  HB32Scheduler(const MachineSchedContext *C): GenericScheduler(C) {}
+  SUnit *pickNode (bool &IsTopNode) override;
+};
+
+} // end namespace llvm
+
+#endif // LLVM_LIB_TARGET_RISCV_VANILLASCHEDULER_H
